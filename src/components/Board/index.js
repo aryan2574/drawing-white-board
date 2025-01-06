514c3ff1078c6e60aded7998259f087a1d8de492
aryan2574 <ayush.aryan94@yahoo.com>
2025-01-06 09:52:25 +0100

    Initial implementation of drawing board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Board = () => {
     const canvasRef = useRef(null);
+    const shouldDraw = useRef(false);
+
     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
     const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
 
@@ -15,9 +17,51 @@ const Board = () => {
 
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+
+        const handleMouseDown = (e) => {
+            shouldDraw.current = true;
+
+            ctx.beginPath();
+            ctx.moveTo(e.clientX, e.clientY);
+        }
+
+        const handleMouseMove = (e) => {
+            if (!shouldDraw.current) return;
+
+            ctx.lineTo(e.clientX, e.clientY);
+            ctx.stroke();
+        }
+
+        const handleMouseUp = () => {
+            shouldDraw.current = false;
+
+            ctx.closePath();
+        }
+
+        canvas.addEventListener("mousedown", handleMouseDown);
+        canvas.addEventListener("mousemove", handleMouseMove);
+        canvas.addEventListener("mouseup", handleMouseUp);
+
+        return () => {
+            canvas.removeEventListener("mousedown", handleMouseDown);
+            canvas.removeEventListener("mousemove", handleMouseMove);
+            canvas.removeEventListener("mouseup", handleMouseUp);
+        }
     }, []);
 
-    console.log(color, size);
+    useEffect(() => {
+        if (!canvasRef.current) return;
+
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        const changeConfig = () => {
+            ctx.strokeStyle = color;
+            ctx.lineWidth = size;
+        }
+
+        changeConfig();
+    }, [color, size]);
 
     return (
         <canvas ref={canvasRef} width={800} height={800} />
